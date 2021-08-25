911cbed255bcaaba0278f63d528da0bb8b2e9835
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-08-25 14:42:32 +0800

    fix: [1.0.2] fix max-height style

--- docs/zhihu.user.js
+++ docs/zhihu.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         知乎移动端修复
-// @version      1.0.1
+// @version      1.0.2
 // @description  知乎移动端修复：阻止跳转，自动展开
 // @author       Allan Chain
 // @homepage     https://github.com/AllanChain/zhihu-mobile
@@ -30,6 +30,6 @@ html, body.ModalWrap-body {
   display: none;
 }
 .RichContent-inner {
-  max-height: fit-content !important;
+  max-height: unset !important;
 }
 `;function o(e){e.classList.remove("is-collapsed")}function s(e){e.querySelectorAll(".RichContent.is-collapsed").forEach(o)}console.log("\u77E5\u4E4E\u79FB\u52A8\u7AEF\u4FEE\u590D\u6B63\u5728\u8FD0\u884C");GM_addStyle(a);var d=new MutationObserver(e=>{for(let t of e)if(t.target.classList.contains("RichContent")&&t.target.classList.contains("is-collapsed"))o(t.target);else for(let n in t.addedNodes)n.nodeType===Node.ELEMENT_NODE&&n.classList.contains("RichContent")&&o(n)});window.addEventListener("load",function(e){e.stopImmediatePropagation(),s(document),d.observe(document.documentElement,{childList:!0,subtree:!0})});document.addEventListener("click",e=>{e.target.tagName!=="BUTTON"&&e.stopImmediatePropagation()},!1);
